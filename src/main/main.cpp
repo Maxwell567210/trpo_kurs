3de69b76540d5ac586ade894f4ff886cca6de048
Max Parfenov <maks-parfenov-98@mail.ru>
2023-10-08 05:56:52 +0700

    Delete struct destructor.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -39,11 +39,11 @@ struct DrawSprite {
     }
 
     // struct destruct
-    ~DrawSprite()
-    {
-        delete texture;
-        delete sprite;
-    }
+    // ~DrawSprite()
+    // {
+    //     delete texture;
+    //     delete sprite;
+    // }
 };
 
 
@@ -121,6 +121,8 @@ int foundWinner(int counter, int* currentField, int FIELD_SIZE, bool &isPlay){
         return 1;
     if(counter == -3)
         return -1;
+
+    return 0;
 }
 
 
@@ -130,6 +132,202 @@ bool checkClick(int pressedX, int pressedY, int xStart, int xEnd, int yStart, in
     else
         return 0;
 }
+
+
+sf::RectangleShape getSpriteCat(
+    int index,
+    DrawSprite pos0Cat,
+    DrawSprite pos1Cat,
+    DrawSprite pos2Cat,
+    DrawSprite pos3Cat,
+    DrawSprite pos4Cat,
+    DrawSprite pos5Cat,
+    DrawSprite pos6Cat,
+    DrawSprite pos7Cat,
+    DrawSprite pos8Cat
+) {
+    switch (index)
+    {
+    case 0:
+        return *pos0Cat.sprite;
+    case 1:
+        return *pos1Cat.sprite;
+    case 2:
+        return *pos2Cat.sprite;
+    case 3:
+        return *pos3Cat.sprite;
+    case 4:
+        return *pos4Cat.sprite;
+    case 5:
+        return *pos5Cat.sprite;
+    case 6:
+        return *pos6Cat.sprite;
+    case 7:
+        return *pos7Cat.sprite;
+    case 8:
+        return *pos8Cat.sprite;
+    }
+
+    return *pos0Cat.sprite;
+}
+
+
+sf::RectangleShape getSpriteLove(
+    int index,
+    DrawSprite pos0Love,
+    DrawSprite pos1Love,
+    DrawSprite pos2Love,
+    DrawSprite pos3Love,
+    DrawSprite pos4Love,
+    DrawSprite pos5Love,
+    DrawSprite pos6Love,
+    DrawSprite pos7Love,
+    DrawSprite pos8Love
+) {
+    switch (index)
+    {
+    case 0:
+        return *pos0Love.sprite;
+    case 1:
+        return *pos1Love.sprite;
+    case 2:
+        return *pos2Love.sprite;
+    case 3:
+        return *pos3Love.sprite;
+    case 4:
+        return *pos4Love.sprite;
+    case 5:
+        return *pos5Love.sprite;
+    case 6:
+        return *pos6Love.sprite;
+    case 7:
+        return *pos7Love.sprite;
+    case 8:
+        return *pos8Love.sprite;
+    }
+
+    return *pos0Love.sprite;
+}
+
+
+void drawOutSprites(
+    int* currentField,
+    RenderWindow &window,
+    int FIELD_SIZE,
+
+    DrawSprite pos0Cat,
+    DrawSprite pos0Love,
+
+    DrawSprite pos1Cat,
+    DrawSprite pos1Love,
+
+    DrawSprite pos2Cat,
+    DrawSprite pos2Love,
+
+    DrawSprite pos3Cat,
+    DrawSprite pos3Love,
+
+    DrawSprite pos4Cat,
+    DrawSprite pos4Love,
+
+    DrawSprite pos5Cat,
+    DrawSprite pos5Love,
+
+    DrawSprite pos6Cat,
+    DrawSprite pos6Love,
+
+    DrawSprite pos7Cat,
+    DrawSprite pos7Love,
+
+    DrawSprite pos8Cat,
+    DrawSprite pos8Love
+    ){
+    for(int index = 0; index < FIELD_SIZE; index++){
+
+        if(currentField[index] == 1){
+
+            window.draw(getSpriteCat(index, pos0Cat, pos1Cat, pos2Cat, pos3Cat, pos4Cat, pos5Cat, pos6Cat, pos7Cat, pos8Cat));
+        }
+
+        if(currentField[index] == -1){
+
+            window.draw(getSpriteLove(index, pos0Love, pos1Love, pos2Love, pos3Love, pos4Love, pos5Love, pos6Love, pos7Love, pos8Love));
+        }
+    }
+
+    // if(currentField[index] == 1){
+    //         window.draw(*pos0Cat.sprite);
+    //     }
+    //     if(currentField[index] == -1){
+    //         window.draw(*pos0Love.sprite);
+    //     }
+    //     index++;
+
+    //     if(currentField[index] == 1){
+    //         window.draw(*pos1Cat.sprite);
+    //     }
+    //     if(currentField[index] == -1){
+    //         window.draw(*pos1Love.sprite);
+    //     }
+    //     index++;
+
+    //     if(currentField[index] == 1){
+    //         window.draw(*pos2Cat.sprite);
+    //     }
+    //     if(currentField[index] == -1){
+    //         window.draw(*pos2Love.sprite);
+    //     }
+    //     index++;
+
+    //     if(currentField[index] == 1){
+    //         window.draw(*pos3Cat.sprite);
+    //     }
+    //     if(currentField[index] == -1){
+    //         window.draw(*pos3Love.sprite);
+    //     }
+    //     index++;
+
+    //     if(currentField[index] == 1){
+    //         window.draw(*pos4Cat.sprite);
+    //     }
+    //     if(currentField[index] == -1){
+    //         window.draw(*pos4Love.sprite);
+    //     }
+    //     index++;
+
+    //     if(currentField[index] == 1){
+    //         window.draw(*pos5Cat.sprite);
+    //     }
+    //     if(currentField[index] == -1){
+    //         window.draw(*pos5Love.sprite);
+    //     }
+    //     index++;
+
+    //     if(currentField[index] == 1){
+    //         window.draw(*pos6Cat.sprite);
+    //     }
+    //     if(currentField[index] == -1){
+    //         window.draw(*pos6Love.sprite);
+    //     }
+    //     index++;
+
+    //     if(currentField[index] == 1){
+    //         window.draw(*pos7Cat.sprite);
+    //     }
+    //     if(currentField[index] == -1){
+    //         window.draw(*pos7Love.sprite);
+    //     }
+    //     index++;
+        
+    //     if(currentField[index] == 1){
+    //         window.draw(*pos8Cat.sprite);
+    //     }
+    //     if(currentField[index] == -1){
+    //         window.draw(*pos8Love.sprite);
+    //     }
+}
+
+
 
 // main
 int main()
@@ -463,78 +661,90 @@ int main()
         }
 
         // draw sprites
-        int index = 0;
 
-        if(currentField[index] == 1){
-            window.draw(*pos0Cat.sprite);
-        }
-        if(currentField[index] == -1){
-            window.draw(*pos0Love.sprite);
-        }
-        index++;
+        drawOutSprites(
+            currentField, window, FIELD_SIZE,
+            pos0Cat, pos0Love,
+            pos1Cat, pos1Love,
+            pos2Cat, pos2Love,
+            pos3Cat, pos3Love,
+            pos4Cat, pos4Love,
+            pos5Cat, pos5Love,
+            pos6Cat, pos6Love,
+            pos7Cat, pos7Love,
+            pos8Cat, pos8Love
+        );
 
-        if(currentField[index] == 1){
-            window.draw(*pos1Cat.sprite);
-        }
-        if(currentField[index] == -1){
-            window.draw(*pos1Love.sprite);
-        }
-        index++;
+        // if(currentField[index] == 1){
+        //     window.draw(*pos0Cat.sprite);
+        // }
+        // if(currentField[index] == -1){
+        //     window.draw(*pos0Love.sprite);
+        // }
+        // index++;
 
-        if(currentField[index] == 1){
-            window.draw(*pos2Cat.sprite);
-        }
-        if(currentField[index] == -1){
-            window.draw(*pos2Love.sprite);
-        }
-        index++;
+        // if(currentField[index] == 1){
+        //     window.draw(*pos1Cat.sprite);
+        // }
+        // if(currentField[index] == -1){
+        //     window.draw(*pos1Love.sprite);
+        // }
+        // index++;
 
-        if(currentField[index] == 1){
-            window.draw(*pos3Cat.sprite);
-        }
-        if(currentField[index] == -1){
-            window.draw(*pos3Love.sprite);
-        }
-        index++;
+        // if(currentField[index] == 1){
+        //     window.draw(*pos2Cat.sprite);
+        // }
+        // if(currentField[index] == -1){
+        //     window.draw(*pos2Love.sprite);
+        // }
+        // index++;
 
-        if(currentField[index] == 1){
-            window.draw(*pos4Cat.sprite);
-        }
-        if(currentField[index] == -1){
-            window.draw(*pos4Love.sprite);
-        }
-        index++;
+        // if(currentField[index] == 1){
+        //     window.draw(*pos3Cat.sprite);
+        // }
+        // if(currentField[index] == -1){
+        //     window.draw(*pos3Love.sprite);
+        // }
+        // index++;
 
-        if(currentField[index] == 1){
-            window.draw(*pos5Cat.sprite);
-        }
-        if(currentField[index] == -1){
-            window.draw(*pos5Love.sprite);
-        }
-        index++;
+        // if(currentField[index] == 1){
+        //     window.draw(*pos4Cat.sprite);
+        // }
+        // if(currentField[index] == -1){
+        //     window.draw(*pos4Love.sprite);
+        // }
+        // index++;
 
-        if(currentField[index] == 1){
-            window.draw(*pos6Cat.sprite);
-        }
-        if(currentField[index] == -1){
-            window.draw(*pos6Love.sprite);
-        }
-        index++;
+        // if(currentField[index] == 1){
+        //     window.draw(*pos5Cat.sprite);
+        // }
+        // if(currentField[index] == -1){
+        //     window.draw(*pos5Love.sprite);
+        // }
+        // index++;
 
-        if(currentField[index] == 1){
-            window.draw(*pos7Cat.sprite);
-        }
-        if(currentField[index] == -1){
-            window.draw(*pos7Love.sprite);
-        }
-        index++;
+        // if(currentField[index] == 1){
+        //     window.draw(*pos6Cat.sprite);
+        // }
+        // if(currentField[index] == -1){
+        //     window.draw(*pos6Love.sprite);
+        // }
+        // index++;
+
+        // if(currentField[index] == 1){
+        //     window.draw(*pos7Cat.sprite);
+        // }
+        // if(currentField[index] == -1){
+        //     window.draw(*pos7Love.sprite);
+        // }
+        // index++;
         
-        if(currentField[index] == 1){
-            window.draw(*pos8Cat.sprite);
-        }
-        if(currentField[index] == -1){
-            window.draw(*pos8Love.sprite);
-        }
+        // if(currentField[index] == 1){
+        //     window.draw(*pos8Cat.sprite);
+        // }
+        // if(currentField[index] == -1){
+        //     window.draw(*pos8Love.sprite);
+        // }
 
 
         // check winner
@@ -574,6 +784,54 @@ int main()
         
         window.display();
     }
+    // DrawSprite background = DrawSprite( RES_PATH + "Background.jpg", Rectangle(0, 0, WIDTH, HEIGHT));
+    // DrawSprite playButton = DrawSprite( RES_PATH + "PLAY.png", Rectangle(1333, 267, 1726, 357));
+    // DrawSprite resetButton = DrawSprite( RES_PATH + "RESET.png", Rectangle(1333, 267, 1726, 357));
+    // DrawSprite BigCatCurrent = DrawSprite( RES_PATH + "BigCat.png", Rectangle(1374, 596, 1694, 900));
+    // DrawSprite BigLoveCurrent = DrawSprite( RES_PATH + "BigLove.png", Rectangle(1374, 596, 1694, 900));
+    // DrawSprite catsWins = DrawSprite( RES_PATH + "cats_win.png", Rectangle(486, 260, 1486, 760));
+    // DrawSprite loveWins = DrawSprite( RES_PATH + "love_win.png", Rectangle(486, 260, 1486, 760));
+
+    // delete[] background.sprite;
+    // delete[] background.texture;
+    
+    // delete[] playButton.sprite;
+    // delete[] playButton.texture;
+
+    // delete[] resetButton.sprite;
+    // delete[] resetButton.texture;
+
+    // delete[] BigCatCurrent.sprite;
+    // delete[] BigCatCurrent.texture;
+
+    // delete[] BigLoveCurrent.sprite;
+    // delete[] BigLoveCurrent.texture;
+
+    // delete[] catsWins.sprite;
+    // delete[] catsWins.texture;
+
+    // delete[] loveWins.sprite;
+    // delete[] loveWins.texture;
+
+    // delete[] pos0Cat.sprite;
+    // delete[] pos0Cat.texture;
+
+    // delete[] pos1Cat.sprite;
+    // delete[] pos1Cat.texture;
+
+    // delete[] pos2Cat.sprite;
+    // delete[] pos2Cat.texture;
+
+    // delete[] pos3Cat.sprite;
+    // delete[] pos3Cat.texture;
+
+    // delete[] pos4Cat.sprite;
+    // delete[] pos4Cat.texture;
+
+    // delete[] pos5Cat.sprite;
+    // delete[] pos5Cat.texture;
+
+    
 
     delete[] currentField;
 }
